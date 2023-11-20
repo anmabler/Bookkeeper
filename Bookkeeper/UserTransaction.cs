15a11f21c1e98c25b0d65d83243fe9ba3b169016
Martina <a.martina.eriksson@hotmail.com>
2023-11-20 11:00:45 +0100

    feat: method for editing a transaciton

---
+++ Bookkeeper/UserTransaction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookkeeper
+{
+    internal class UserTransaction
+    {
+        private string title;
+        private int amount;
+        private int month;
+        private bool isIncome;
+
+        // Getters and setters
+
+        public int getAmount()
+        {
+            return amount;
+        }
+        public int getMonth()
+        {
+            return month;
+        }
+        public string getTitle()
+        {
+            return title;
+        }
+        
+        public void editTransaction(string title, int amount, int month, bool isIncome)
+        {
+            this.title = title;
+            this.amount = amount;
+            this.month = month;
+            this.isIncome = isIncome;
+
+        }
+
+        public void setAmount(int amount)
+        {
+            this.amount = amount;
+        }
+        public void setMonth(int month)
+        {
+            this.month = month;
+        }
+        public void setisIncome(bool isIncome)
+        {
+            this.isIncome = isIncome;
+        }
+        public void setTitle(string title)
+        {
+            this.title = title; 
+        }
+    }
+}
