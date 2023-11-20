822e49b80b4ac1858f491d7ef02718ea994b51fb
Martina <a.martina.eriksson@hotmail.com>
2023-11-20 15:48:56 +0100

    feat: TransactionList class w methods

--- Bookkeeper/Program.cs
+++ Bookkeeper/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Bookkeeper;
+
+TransactionList transactionList = new TransactionList();
+transactionList.userTransactionList.AddRange(new List<UserTransaction>
+{
+    new UserTransaction("Köpa skor", 800, 11, false),
+    new UserTransaction("Oktober lön", 849, 10, true),
+    new UserTransaction("Barnbidrag", 650, 10, true),
+    new UserTransaction("Julklappar", 2000, 11, false),
+
+});
+
+//transactionList.displayExpense();
+transactionList.displayIncome();
+//transactionList.displayAllTransactions();
---
+++ Bookkeeper/TransactionList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookkeeper
+{
+    internal class TransactionList
+    {
+        public List<UserTransaction> userTransactionList = new List<UserTransaction>();
+
+        public void displayIncome()
+        {
+            var incomeList = userTransactionList.Where(transaction => transaction.getIsIncome() == true).ToList();
+            transactionOutput(incomeList);
+        }
+
+        public void displayExpense()
+        {
+            var expenseList = userTransactionList.Where(transaction => transaction.getIsIncome() == false).ToList();
+
+            transactionOutput(expenseList);
+        }
+
+        public void displayAllTransactions()
+        {
+            transactionOutput(userTransactionList);
+        }
+
+        private void transactionOutput(List<UserTransaction> list)
+        {
+            foreach (var transaction in list)
+            {
+                Console.WriteLine($"{transaction.getTitle()} {transaction.getAmount()}  {transaction.getMonth()}  {transaction.getIsIncome()}");
+
+            }
+        }
+    }
+}
